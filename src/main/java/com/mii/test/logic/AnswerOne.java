759ba2a2f4e6433d588fae99833119b288027463
ariobimo74@gmail.com <ariobimo74@gmail.com>
2021-08-05 19:55:42 +0700

    number 1 answered

---
+++ src/main/java/com/mii/test/logic/AnswerOne.java
@@ -0,0 +1,39 @@
+package main.java.com.mii.test.logic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AnswerOne
+{
+    public List<String> answerList(int numberCount)
+    {
+        List<String> resultList = new ArrayList<>();
+
+        for (int i = 1; i <= numberCount; i++)
+        {
+            String result = String.valueOf(i);
+            if ((i%3 == 0) && (i%4 == 0))
+            {
+                result = "OKYES";
+            }
+            else
+            {
+                if (i%3 == 0)
+                {
+                    result = "OK";
+                }
+                else
+                {
+                    if (i%4 == 0)
+                    {
+                        result = "YES";
+                    }
+                }
+            }
+
+            resultList.add(result);
+        }
+
+        return resultList;
+    }
+}
--- src/main/java/com/mii/test/logic/MainAnswers.java
+++ src/main/java/com/mii/test/logic/MainAnswers.java
@@ -2,7 +2,9 @@ package main.java.com.mii.test.logic;
 
 public class MainAnswers
 {
-    public static void main(String[] args) {
-        System.out.println("Test");
+    public static void main(String[] args)
+    {
+        AnswerOne answerOne = new AnswerOne();
+        System.out.println("answerOne.answerList(15) = " + answerOne.answerList(15));
     }
 }
